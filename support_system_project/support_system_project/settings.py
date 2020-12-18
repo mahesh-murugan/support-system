22d08a6f26d0d36bd64bf079ff06b8dc1a5faabe
mahesh <mahesh@elementzonline.in>
2020-12-18 19:22:16 +0530

    security fix

--- support_system_project/support_system_project/settings.py
+++ support_system_project/support_system_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^3xlogy2lx5qsiygurd^8i(je45q!mkfm11t)zh1-wn78v8log'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
